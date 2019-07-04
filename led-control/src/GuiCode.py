234587cd8c5bf3922b4dd1168b35abf6c2e101bb
luis.garcia <luis.garcia@uni-muenster.de>
2019-07-04 16:35:49 +0200

    updates gui

--- led-control/src/GuiCode.py
+++ led-control/src/GuiCode.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'GuiCode.ui',
 # licensing of 'GuiCode.ui' applies.
 #
-# Created: Thu Jul  4 12:53:16 2019
+# Created: Thu Jul  4 16:35:47 2019
 #      by: pyside2-uic  running on PySide2 5.12.4
 #
 # WARNING! All changes made in this file will be lost!
